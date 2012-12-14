4479bcec8d5dc1eeea5d59e808269ac2d7f89d4b
Willian Oizumi <oizumi.willian@gmail.com>
2012-12-14 21:46:53 -0200

    initial commit

---
+++ nome.rb
@@ -0,0 +1,32 @@
+﻿class Nome
+
+	VALOR_PADRAO = 13
+
+	def self.calcula_distancia(nome1, nome2)
+		distancia = 0	
+		limite = [nome1.size, nome2.size].max - 1
+		(0..limite).each do |i|
+			distancia += internal_calcula_distancia(nome1[i], nome2[i])
+		end
+
+		distancia
+	end
+
+	def self.internal_calcula_distancia(char1, char2)
+		return VALOR_PADRAO if char1 == nil || char2 == nil 
+		return (asc_ignore_case(char1) - asc_ignore_case(char2)).abs
+	end
+
+	def self.asc_ignore_case(palavra)
+		palavra.upcase.bytes.to_a[0]
+	end
+
+	def self.calcula_nome(nome_base, nomes)
+		distancias = {}
+		nomes.each do |nome|
+			distancia = calcula_distancia(nome_base, nome)
+			distancias[distancia] = nome
+		end
+		distancias[distancias.keys.min]
+	end
+end
---
+++ nome_spec.rb
@@ -0,0 +1,51 @@
+﻿require_relative "nome"
+
+describe "Escolha o nome do seu filho" do 
+	context "Calcula a distancia entre duas palavras" do		
+		it "A distancia entre A e A deve ser 0" do
+			Nome.calcula_distancia("A", "A").should == 0
+		end	
+
+		it "A distancia entre A e B deve ser 1" do
+			Nome.calcula_distancia("A", "B").should == 1
+		end
+
+		it "A distancia entre C e B deve ser 1" do
+			Nome.calcula_distancia("C", "B").should == 1
+		end
+
+		it "A distancia entre a e A deve ser 0" do
+			Nome.calcula_distancia("a", "A").should == 0
+		end
+
+		it "A distancia entre Z e a deve ser 25" do
+			Nome.calcula_distancia("Z", "a").should == 25
+		end
+
+		it "A distancia entre AA e AA deve ser 0" do
+			Nome.calcula_distancia("AA", "AA").should == 0
+		end
+
+		it "A distancia entre AA e AC deve ser 2" do
+			Nome.calcula_distancia("AA", "AC").should == 2
+		end
+
+		it "A distancia entre ABC e AB deve ser 13" do
+			Nome.calcula_distancia("ABC", "AB").should == 13
+		end
+
+		it "A distancia entre ab e ABC deve ser 13" do
+			Nome.calcula_distancia("ab", "ABC").should == 13
+		end
+	end
+
+	context "Calcula nome menos distante" do
+		it "Para [Amanda, Bruna, Camila] com palavra_base Jose deve escolher Bruna" do
+			Nome.calcula_nome("Jose", ["Amanda", "Bruna", "Camila"]).should == "Bruna"
+		end
+
+		it "Para [Amanda, Bruna, Camila] com palavra_base Camila deve escolher Camila" do
+			Nome.calcula_nome("Camila", ["Amanda", "Bruna", "Camila"]).should == "Camila"
+		end
+	end
+end
